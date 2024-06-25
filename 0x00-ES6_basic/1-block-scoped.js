15560db68e6ab1ab1df43e51ff0297cc6b20fb09
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-06-25 03:35:18 +0100

    jj

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlocks(t
+rueOrFalse) {
+    const task = false;
+    connst task2 = true;
+
+    if (trueOFalse) {
+        var task = true;
+        var task2 = false;
+    }
+
+    return [task, task2];
+}
