439b99134bc5c89c518a3db443df546a0ee23f68
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-08-22 01:51:55 +0100

    kk

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,78 +1,48 @@
-// Import the 'readFile' function from the 'fs' module to handle file reading asynchronously
-const { readFile } = require('fs');
+const fs = require('fs');
 
 /**
- * Function to count the number of students in a CSV file and group them by their fields of study.
- * 
- * @param {string} fileName - The name of the CSV file to read.
- * @returns {Promise} - A promise that resolves when the file has been read and the student data has been processed.
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
  */
-function countStudents(fileName) {
-  // Object to store the list of students by field
-  const students = {};
-  
-  // Object to store the count of students by field
-  const fields = {};
-  
-  // Variable to keep track of the total number of students
-  let length = 0;
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  fs.readFile(dataPath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const fileLines = data
+        .toString('utf-8')
+        .trim()
+        .split('\n');
+      const studentGroups = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames
+        .slice(0, dbFieldNames.length - 1);
 
-  // Return a promise that will handle the asynchronous file reading and processing
-  return new Promise((resolve, reject) => {
-    // Read the file asynchronously
-    readFile(fileName, (error, data) => {
-      if (error) {
-        // If an error occurs, reject the promise with an appropriate error message
-        reject(Error('Cannot load the database'));
-      } else {
-        // Split the file content by line into an array
-        const lines = data.toString().split('\n');
-        
-        // Iterate over each line in the file
-        for (let i = 0; i < lines.length; i += 1) {
-          // Only process non-empty lines
-          if (lines[i]) {
-            // Increment the total student counter
-            length += 1;
-            
-            // Split the line into an array of values (fields)
-            const field = lines[i].toString().split(',');
-            
-            // Add the student's name to the list for their field
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-            
-            // Increment the count for the student's field
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
-            } else {
-              fields[field[3]] = 1;
-            }
-          }
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord
+          .slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
         }
-
-        // Subtract 1 to account for the header row in the CSV file
-        const l = length - 1;
-        
-        // Log the total number of students (excluding the header)
-        console.log(`Number of students: ${l}`);
-        
-        // Log the number of students and the list of their names for each field
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {  // Skip the header row
-            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-          }
-        }
-
-        // Resolve the promise with the file data
-        resolve(data);
+        const studentEntries = studentPropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
       }
-    });
-  });
-}
 
-// Export the countStudents function for use in other modules
+      const totalStudents = Object
+        .values(studentGroups)
+        .reduce((pre, cur) => (pre || []).length + cur.length);
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, group] of Object.entries(studentGroups)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+      }
+      resolve(true);
+    }
+  });
+});
+
 module.exports = countStudents;
